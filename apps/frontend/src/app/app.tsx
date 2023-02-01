8d8b37d9ea648cb49104360ba9e9872e4c0c5c9e
Prasad Lakshan <kpdplakshan@gmail.com>
2023-02-01 17:55:50 +0530

    add express api and api in front end

--- apps/frontend/src/app/app.tsx
+++ apps/frontend/src/app/app.tsx
@@ -1,13 +1,72 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import styles from './app.module.scss';
 import NxWelcome from './nx-welcome';
+import styled from 'styled-components';
 
-export function App() {
+const StyledApp = styled.div`
+  font-family: sans-serif;
+  min-width: 300px;
+  max-width: 600px;
+  margin: 50px auto;
+  .flex {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+  header {
+    background-color: #143055;
+    color: white;
+    padding: 5px;
+    border-radius: 3px;
+  }
+  main {
+    padding: 0 36px;
+  }
+  h1 {
+    text-align: center;
+    margin-left: 18px;
+    font-size: 24px;
+  }
+  .ticket {
+    color: #0094ba;
+    height: 36px;
+    background-color: rgba(0, 0, 0, 0);
+    border: 1px solid rgba(0, 0, 0, 0.12);
+    border-radius: 4px;
+  }
+`;
+
+type Ticket = {
+  title: string;
+  id: number;
+};
+
+const App = () => {
+
+  const tickets: Ticket[] = [
+    {
+      title: `Install updates`,
+      id: 1
+    },
+    {
+      title: `Restore the backup`,
+      id: 2
+    }
+  ];
+
   return (
-    <>
-      <NxWelcome title="frontend" />
-      <div />
-    </>
+    <StyledApp>
+      <header className="flex">
+        <h1>Welcome to tickets!</h1>
+      </header>
+      <main>
+        { tickets.map(t => (
+          <p className="ticket flex" key={ t.id }>
+            { t.title }
+          </p>
+        )) }
+      </main>
+    </StyledApp>
   );
 }
 
